b84cb83c98c7184249813a1ef873cc9d48cd12c9
Yaheni1992 <neo_jeka@mail.ru>
2021-11-25 00:06:24 +0300

    lesson3

--- src/Lesson3/Lesson3.java
+++ src/Lesson3/Lesson3.java
@@ -3,6 +3,16 @@ package Lesson3;
 public class Lesson3 {
     public static void main(String[] args) {
 
+//                    1 дз
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+            System.out.print(arr[i] + " ");
+        }
     }
-
 }
+
