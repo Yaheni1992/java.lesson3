c5033df7e1f77818bcb01318a53d8509ef9dfec2
Yaheni1992 <neo_jeka@mail.ru>
2021-11-25 00:15:24 +0300

    lesson3. доделал 2,3

--- src/Lesson3/Lesson3.java
+++ src/Lesson3/Lesson3.java
@@ -1,9 +1,8 @@
 package Lesson3;
 
 public class Lesson3 {
+//                  1дз
     public static void main(String[] args) {
-
-//                    1 дз
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
@@ -14,5 +13,30 @@ public class Lesson3 {
             System.out.print(arr[i] + " ");
         }
     }
+
+
+//                      2 дз
+        public static void fillArray() {
+            int[] arr = new int[100];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = i;
+                System.out.print(arr[i] + " ");
+            }
+
+        }
+    //                 3 Дз
+    public static void changeArray(String[] args) {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] <= 6) {
+                arr[i] *= 2;
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
 }
+
+
+
+
 
